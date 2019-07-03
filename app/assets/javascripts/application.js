1d5ba36801b3a431ce7c93592b96edc436217d30
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-07-03 17:55:55 +0900

    done index view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,11 @@
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require_tree .
 
 $(function () {
   $('[data-toggle="popover"]').popover()
-})
+});
